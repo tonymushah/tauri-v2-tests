34bfeb96cde294eb539de6705660727c469ea90e
tonymushah <tonyramanantsalama@gmail.com>
2024-12-12 11:03:08 +0300

    fixed `EventRedirect.svelte`

--- src/lib/components/EventRedirect.svelte
+++ src/lib/components/EventRedirect.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
 	import listenStore from '$lib/utils/listenerStore';
+	import { text } from '@sveltejs/kit';
 	import { emit } from '@tauri-apps/api/event';
-
-	const someEvent = listenStore<string | undefined>('my-event', undefined);
-	let inputText = $state<string>('');
+	type MyEventPayload = {
+		text: string;
+	};
+	const someEvent = listenStore<MyEventPayload | undefined>('my-event', undefined);
+	let inputText = $state<string | undefined>(undefined);
 	$effect(() => {
-		emit('my-event', inputText).catch(console.error);
+		if (inputText != undefined && inputText?.length != 0) {
+			emit('my-event', {
+				text: inputText
+			}).catch(console.error);
+		}
 	});
 </script>
 
 <textarea placeholder="my-event input lol" bind:value={inputText}></textarea>
 
 {#if $someEvent}
-	<p>{$someEvent}</p>
+	<p>{$someEvent.text}</p>
 {/if}
