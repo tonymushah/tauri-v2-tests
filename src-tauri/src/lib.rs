3d366cdc92210ab7f3aed451e4ca33546115745f
tonymushah <tonyramanantsalama@gmail.com>
2024-12-12 11:02:43 +0300

    `my-event` listened from rust

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,22 +1,27 @@
+use tauri::Listener;
+
 #[tauri::command]
 async fn say(name: String) -> String {
-  format!("Hello {name}")
+    format!("Hello {name}")
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![say])
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![say])
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            app.listen("my-event", |e| {
+                println!("my-event: {}", e.payload());
+            });
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
